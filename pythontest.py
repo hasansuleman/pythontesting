cccd414b73126b57763ada9721db393e75b7b57e
hasansuleman1 <hasansuleman1@gmail.com>
2017-06-25 18:55:23 -0700

    conflicting file

--- pythontest.py
+++ pythontest.py
@@ -1,4 +1,6 @@
 #this is for the changes in the second project
 print('this is the third change')
 print('hello')
+print('added by H1')
 #adding more stuff
+#adding stuff make sure it doesnt conflict
